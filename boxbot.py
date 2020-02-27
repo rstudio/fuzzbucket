78374bdc2cec759851401fb0f240428a09e866b4
Dan Buch <dan@meatballhat.com>
2020-02-26 21:00:50 -0500

    Fill in more ec2 things

--- boxbot.py
+++ boxbot.py
@@ -1,11 +1,15 @@
+import base64
 import json
 import logging
 import os
 import sys
+import time
 
 import boto3
 
 from botocore.exceptions import ClientError
+
+TAGS = {"USER": "boxbot:user", "CREATED_AT": "boxbot:created-at"}
 
 
 log = logging.getLogger(__name__)
@@ -18,14 +22,21 @@ logging.basicConfig(
 
 
 def list_boxes(event, context, client=None):
-    log.debug(f"received event={_to_json(event)}")
-    client = boto3.client("ec2") if client is None else client
-    body = {"instances": {}}
+    client = client if client is not None else boto3.client("ec2")
+
+    user = _extract_user(event)
+    if user is None:
+        return {
+            "statusCode": 403,
+            "body": _to_json("no user found"),
+        }
+
+    body = {"instances": []}
     try:
-        filters = [{"Name": "tag-key", "Values": ["boxbot:created-at"]}]
-        user = _q(event, "user")
-        if user is not None:
-            filters.append({"Name": "tag:boxbot:user", "Values": [user]})
+        filters = [
+            {"Name": "tag-key", "Values": [TAGS["CREATED_AT"]]},
+            {"Name": f'tag:{TAGS["USER"]}', "Values": [user]},
+        ]
         body["instances"] = _describe_instances(client, Filters=filters)
         return {"statusCode": 200, "body": _to_json(body)}
     except ClientError:
@@ -38,7 +49,53 @@ def show_box(event, context, client=None):
 
 
 def create_box(event, context, client=None):
-    return {"statusCode": 501, "body": '"not implemented"'}
+    client = client if client is not None else boto3.client("ec2")
+
+    user = _extract_user(event)
+    if user is None:
+        return {
+            "statusCode": 403,
+            "body": _to_json("no user found"),
+        }
+
+    image_alias = _q(event, "image_alias", "ubuntu18")
+    ami = _resolve_ami_alias(image_alias, None)
+    if ami is None:
+        return {
+            "statusCode": 400,
+            "body": _to_json(f"unknown image_alias={image_alias}"),
+        }
+
+    try:
+        response = client.run_instances(
+            # FIXME: un-hardcodify
+            ImageId=ami,
+            InstanceType="t3.small",
+            KeyName="connect",
+            MinCount=1,
+            MaxCount=1,
+            SecurityGroupIds=["sg-0620d67e0cd3cbdc3"],
+            NetworkInterfaces=[
+                {
+                    "DeviceIndex": 0,
+                    "AssociatePublicIpAddress": True,
+                    "SubnetId": "subnet-0c5015feba8daf817",
+                }
+            ],
+            TagSpecifications=[
+                {
+                    "ResourceType": "instance",
+                    "Tags": [
+                        {"Key": TAGS["USER"], "Value": user},
+                        {"Key": TAGS["CREATED_AT"], "Value": str(time.time())},
+                    ],
+                }
+            ],
+        )
+        return {"statusCode": 200, "body": _to_json(response)}
+    except ClientError:
+        log.exception("oh no")
+        return {"statusCode": 500, "body": _to_json("oh no")}
 
 
 def delete_box(event, context, client=None):
@@ -70,6 +127,25 @@ def _describe_instances(client, *args, **kwargs):
     return list(sorted(instances, key=lambda i: i["name"]))
 
 
+def _resolve_ami_alias(image_alias, default=None):
+    # TODO: dynamically?
+    return {"ubuntu18": "ami-046842448f9e74e7d"}.get(image_alias, default)
+
+
+def _extract_user(event):
+    auth_header = None
+    if "Authorization" in event.get("headers", {}):
+        auth_header = event["headers"]["Authorization"]
+    if "authorization" in event.get("headers", {}):
+        auth_header = event["headers"]["authorization"]
+
+    if auth_header is not None:
+        b64_token = auth_header.split(" ")[-1]
+        return base64.b64decode(b64_token).decode("utf-8").split(":")[0]
+
+    return _q(event, "user")
+
+
 def _q(event, key, default=None):
     if event is None:
         return default
@@ -77,6 +153,3 @@ def _q(event, key, default=None):
     if qs is None:
         return default
     return qs.get(key, default)
-
-
-log.info("boxbot is alive")
--- test_boxbot.py
+++ test_boxbot.py
@@ -1,18 +1,52 @@
+import base64
 import json
 
 import boto3
+import pytest
 
 from moto import mock_ec2
 
 import boxbot
 
 
+@pytest.fixture
+def authd_event():
+    return {
+        "headers": {"Authorization": base64.b64encode(b"pytest:zzz").decode("utf-8")}
+    }
+
+
 @mock_ec2
-def test_list_boxes():
+def test_list_boxes(authd_event):
     client = boto3.client("ec2")
-    response = boxbot.list_boxes({}, None, client=client)
+    response = boxbot.list_boxes(authd_event, None, client=client)
     assert response["statusCode"] == 200
     assert response["body"] is not None
     body = json.loads(response["body"])
     assert "instances" in body
     assert body["instances"] is not None
+
+
+@mock_ec2
+def test_list_boxes_forbidden():
+    client = boto3.client("ec2")
+    response = boxbot.list_boxes({}, None, client=client)
+    assert response["statusCode"] == 403
+
+
+@mock_ec2
+@pytest.mark.skip(reason="subnet problems")
+def test_create_box(authd_event):
+    client = boto3.client("ec2")
+    response = boxbot.create_box(authd_event, None, client=client)
+    assert response["statusCode"] == 200
+    assert response["body"] is not None
+    body = json.loads(response["body"])
+    assert "Instances" in body
+
+
+@mock_ec2
+def test_create_box_forbidden():
+    client = boto3.client("ec2")
+    response = boxbot.create_box({}, None, client=client)
+    assert response["statusCode"] == 403
