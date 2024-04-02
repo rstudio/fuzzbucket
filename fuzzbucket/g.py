3b33bcad46679fcbb8aca249afb247d0c89327ab
Elliot Murphy <statik@users.noreply.github.com>
2024-04-02 14:29:05 -0400

    run black to fix code formatting
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- fuzzbucket/g.py
+++ fuzzbucket/g.py
@@ -7,10 +7,10 @@ oauth_blueprint: werkzeug.local.LocalProxy[
     flask_dance.consumer.OAuth2ConsumerBlueprint
 ] = werkzeug.local.LocalProxy(auth.get_oauth_blueprint)
 
-oauth_session: werkzeug.local.LocalProxy[
-    flask_dance.consumer.OAuth2Session
-] = werkzeug.local.LocalProxy(lambda: auth.get_oauth_blueprint().session)
+oauth_session: werkzeug.local.LocalProxy[flask_dance.consumer.OAuth2Session] = (
+    werkzeug.local.LocalProxy(lambda: auth.get_oauth_blueprint().session)
+)
 
-user_storage: werkzeug.local.LocalProxy[
-    flask_dance_storage.FlaskDanceStorage
-] = werkzeug.local.LocalProxy(flask_dance_storage.get_storage)
+user_storage: werkzeug.local.LocalProxy[flask_dance_storage.FlaskDanceStorage] = (
+    werkzeug.local.LocalProxy(flask_dance_storage.get_storage)
+)
